f13572eac77f913c6aa9870b014d991dca5adfa7
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-16 19:50:09 +0300

    hw08js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,4 @@
-// import Player from '@vimeo/player';
+import Player from '@vimeo/player';
 // import throttle from 'lodash.throttle';
 import SimpleLightbox from 'simplelightbox';
 const CURRENT_TIME_KEY = 'videoplayer-current-time';
